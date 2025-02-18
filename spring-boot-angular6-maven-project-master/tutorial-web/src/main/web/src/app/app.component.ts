3dcd1ccb85e1d91b0779619fa9d90caab7e2353e
Akshata-P-Patil <akshata1597@gmail.com>
2025-02-18 14:36:36 +0530

    removed unwanted files

--- spring-boot-angular6-maven-project-master/tutorial-web/src/main/web/src/app/app.component.ts
+++ spring-boot-angular6-maven-project-master/tutorial-web/src/main/web/src/app/app.component.ts
@@ -7,21 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent{
   downloaded = false ; // Boolean to control whether the text is visible
+  uploadError: boolean;
 
   download() {
     console.log("clicked");
     this.downloaded = !this.downloaded;
   }
 
-  selectedFileName: string | null = null;
+  selectedSwFileName: string | null = null;
+  selectedS19FileName: string | null = null;
+
 
   // Method to handle file selection
-  onFileSelected(event: Event): void {
+  onSwFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
     if (input && input.files) {
       const file = input.files[0];
       if (file) {
-        this.selectedFileName = file.name;
+        this.selectedSwFileName = file.name;
+
+        // handle the file uploaded here
+
+        console.log(file); // For testing, logging the selected file
+      }
+      else{
+        this.uploadError = true;
+      }
+    }
+  }
+
+
+  // Method to handle file selection
+  onS19FileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input && input.files) {
+      const file = input.files[0];
+      if (file) {
+        this.selectedS19FileName = file.name;
 
         // handle the file uploaded here
 
