c4d5dab33b04fb8e9d8894b0c9898cb23906088d
Akshata-P-Patil <akshata1597@gmail.com>
2025-02-20 09:38:29 +0530

    frontend for all functionality done

--- spring-boot-angular6-maven-project-master/tutorial-web/src/main/web/src/app/app.component.ts
+++ spring-boot-angular6-maven-project-master/tutorial-web/src/main/web/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UploadService } from '../service/upload.service';
 
 @Component({
   selector: 'app-root',
@@ -7,15 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent{
   downloaded = false ; // Boolean to control whether the text is visible
-  uploadError: boolean;
+  swuploadSuccess: boolean;
+  swuploadAttempted: boolean;
+  s19uploadAttempted: boolean;
+  s19uploadSuccess: boolean;
+  downloadSuccess: boolean;
+  downloadMessage: string;
+  downloadAttempted: boolean;
+  generateSuccess: boolean;
+  ZipAttempted: boolean;
+  zipMessage: string;
+
+  constructor(private uploadService: UploadService) {}
 
   download() {
     console.log("clicked");
     this.downloaded = !this.downloaded;
   }
 
-  selectedSwFileName: string | null = null;
-  selectedS19FileName: string | null = null;
+  uploadSwFileMessage: any;
+  uploadS19FileMessage: any;
 
 
   // Method to handle file selection
@@ -23,32 +35,101 @@ export class AppComponent{
     const input = event.target as HTMLInputElement;
     if (input && input.files) {
       const file = input.files[0];
-      if (file) {
-        this.selectedSwFileName = file.name;
-
-        // handle the file uploaded here
-
-        console.log(file); // For testing, logging the selected file
+      const formData = new FormData();
+      formData.append('file', file, file.name);
+      if(file.name.includes(".swu")){
+        this.uploadService.uploadSwFile(formData).subscribe({
+          next: (response) => {
+            this.swuploadAttempted = true;
+            this.swuploadSuccess = true;
+            this.uploadSwFileMessage = response.body.message;
+            console.log('Upload successful', response);
+          },
+          error: (error) => {
+            this.swuploadAttempted = true;
+            this.swuploadSuccess = false;
+            this.uploadSwFileMessage = error;
+            console.error('Error uploading file', error);
+          }
+        });
       }
       else{
-        this.uploadError = true;
+        this.swuploadAttempted = true;
+        this.swuploadSuccess = false;
+        this.uploadSwFileMessage = "Error in uploading file";
+      }
+      }
+      else{
+        alert('Please select a file to upload.');
       }
     }
-  }
-
-
-  // Method to handle file selection
+  
   onS19FileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
     if (input && input.files) {
       const file = input.files[0];
-      if (file) {
-        this.selectedS19FileName = file.name;
-
-        // handle the file uploaded here
-
-        console.log(file); // For testing, logging the selected file
+      const formData = new FormData();
+      formData.append('file', file, file.name);
+      if(file.name.includes(".s19")){
+        this.uploadService.uploadS19File(formData).subscribe({
+          next: (response) => {
+            this.s19uploadAttempted = true;
+            this.s19uploadSuccess = true;
+            this.uploadS19FileMessage = response.body.message;
+            console.log('Upload successful', response);
+          },
+          error: (error) => {
+            this.s19uploadAttempted = true;
+            this.s19uploadSuccess = false;
+            this.uploadS19FileMessage =  "Error in uploading file";
+            console.error('Error uploading file', error);
+          }
+        });
       }
-    }
+      else{
+        this.s19uploadAttempted = true;
+        this.s19uploadSuccess = false;
+        this.uploadS19FileMessage = "Error in uploading file";
+      }
+      }
+      else{
+        alert('Please select a file to upload.');
+      }
   }
-}
+
+
+  downloadFile() {
+        this.uploadService.downloadFile().subscribe({
+          next: (response) => {
+           this.downloadSuccess = true;
+           this.downloadAttempted = true;
+           this.downloadMessage = "downloaded successfully"
+            console.log('download successful', response);
+          },
+          error: (error) => {
+            this.downloadSuccess = false;
+            this.downloadAttempted = true;
+            this.downloadMessage =  "Error in downloading, file not found";
+            console.error('Error in downloading file', error);
+          }
+        });
+      }
+
+
+      generateZipFile() {
+        this.uploadService.generateZipFile().subscribe({
+          next: (response) => {
+           this.generateSuccess = true;
+           this.ZipAttempted = true;
+           this.zipMessage = " Zip generated successfully"
+            console.log('zip generated successful', response);
+          },
+          error: (error) => {
+            this.generateSuccess = false;
+            this.ZipAttempted = true;
+            this.zipMessage =  "Error in generating zip file";
+            console.error('Error in generating zip file', error);
+          }
+        });
+      }
+  }
